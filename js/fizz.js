e230cd1c5aa8bb8352afe5d23ac832205b22eb21
Nanoman <nanoman689@gmail.com>
2015-02-27 09:50:08 -0800

    It works so far
    - Enter button works
    - Fizz buzz is working
    - Trying to get it to check the length and if it’s a number.
    - It figures out if it’s a number just find, but not trying to find a
    way for it to check the length of that number.

--- js/fizz.js
+++ js/fizz.js
@@ -2,34 +2,51 @@ $(document).ready(function (){
     /* get the number */
     $("#numMain").keydown(function (event) {
         if (event.which === 13) {
-            var numberGuess = document.getElementById('numMain').value;
+            var numberGuess = +document.getElementById('numMain').value;
             console.log("You number is: " + numberGuess);
-        }
-    }    
-    /* Fizz Buzz time */
 
-    for ( a = 1; a <= numberGuess; a++ ){
-        if (a%3 === 0){
-           if (a%5 === 0){
-                console.log("FizzBuzz")
-            } else {
-                console.log("Fizz")
+            /* check to see if it's a number */
+            var checkNum = $("#numMain").val();
+            if(checkNum.length == 0) {
+                alert("You need to enter a number!");
             }
-        } else if (a%5 === 0){
-            console.log("Buzz")
-        } else {
-            console(a)
+            else {
+                if(jQuery.isNumeric(checkNum)){
+                    console.log("Your number is "+ numberGuess);
+                }
+                else {
+                    alert("That's not a number!");
+                }
+            }    
+
+
+            /* check the length of the entered number */
+
+            /* Fizz Buzz time */
+        
+            for ( a = 1; a <= numberGuess; a++ ){
+                if (a%3 === 0){
+                   if (a%5 === 0){
+                        console.log("FizzBuzz")
+                        $(".numbers").append("FizzBuzz" + "<p></p>");
+                        /* $("").prepend("<p></p>"); */
+
+                    } else {
+                        console.log("Fizz")
+                        $(".numbers").append("Fizz"  + "<p></p>");
+                        
+                    }
+                } else if (a%5 === 0){
+                    console.log("Buzz")
+                    $(".numbers").append("Buzz"  + "<p></p>");
+                    
+
+                } else {
+                    console.log(a)
+                    $(".numbers").append(a  + "<p></p>");
+                    
+                }
+            }
         }
-    }   
-
-    /* check the number 
-    
-    var checkNum = document.getElementById("numMain").value;
-    if (isNaN(checkNum)) {
-        alert(checkNum + "is not a number!");
-    } else {
-        alert(checkNum + "that's a number!");
-    }
-
-    */
+    });
 });
